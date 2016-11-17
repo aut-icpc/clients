458f9e2a4002c18886b8a28525439725ea991af3
Parham Alvani <parham.alvani@gmail.com>
2016-11-17 04:17:11 +0330

    Installation script was improved.

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
 sudo apt install codeblocks
-sudo apt install iptables-persistent
 sudo bash rules.sh
 cp -R Programs ~/
 sudo cp jetbrains-idea.desktop /usr/share/applications
-sudo cp sublime.desktop /usr/share/applications
-cd ~/Programs; tar xvfz ideaUI.tar.gz
+sudo cp jon-sublime.desktop /usr/share/applications
+sudo cp "judging note.pdf" /usr/share/doc/icpc
+sudo cp "TechnicalNotes.pdf" /usr/share/doc/icpc
+cd ~/Programs; tar xvfz ideaUI.tar.gz; tar xvfz sublime2.tar.gz
