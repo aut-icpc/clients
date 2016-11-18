591be5dd410607ddf4f564d98b5c58bb548ceaf1
Parham Alvani <parham.alvani@gmail.com>
2016-11-18 10:20:51 +0330

    Jetbrains needs more ports :)

--- rules.sh
+++ rules.sh
@@ -11,5 +11,5 @@ iptables -F
 iptables -P OUTPUT DROP
 iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -d 192.168.128.0/24 --dport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp -d 127.0.0.0/8 --dport 33494 -j ACCEPT
+iptables -A OUTPUT -p tcp -d 127.0.0.0/8 --dport 1:65535 -j ACCEPT
 iptables-save > /etc/iptables.rules
