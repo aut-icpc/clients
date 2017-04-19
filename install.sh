794a5c0e4a102af14cdd91573ce37f1a4fc68dde
Parham Alvani <parham.alvani@gmail.com>
2017-04-19 20:52:02 +0430

    Installation script was improved.

--- install.sh
+++ install.sh
@@ -7,11 +7,22 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
-sudo apt install codeblocks
+# Login First, Thanks to FAUTLogin
+code=`curl -k -s -o /dev/null -w "%{http_code}" \
+	-X POST \
+	-H "Content-Type: application/x-www-form-urlencoded" \
+	-d "erase-cookie=false&password=$password&popup=false&username=$username" \
+	"https://login.aut.ac.ir/login"`
+
+if [ $code == '302' ]; then
+	echo "Login was successful."
+else
+	echo "Login was failed."
+fi
+
+# Install required packages
+sudo apt-get update
+sudo apt-get install -y codeblocks
+
+# Bans the traffics
 sudo bash rules.sh
-cp -R Programs ~/
-sudo cp jetbrains-idea.desktop /usr/share/applications
-sudo cp jon-sublime.desktop /usr/share/applications
-sudo cp "judging note.pdf" /usr/share/doc/icpc
-sudo cp "TechnicalNotes.pdf" /usr/share/doc/icpc
-cd ~/Programs; tar xvfz ideaUI.tar.gz; tar xvfz sublime2.tar.gz
