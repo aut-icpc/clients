c7f639feead52d035269bfe9a82bb2b19370b90b
smf8 <mr.smf8@gmail.com>
2019-11-07 12:17:57 +0330

    Added printer ip

--- rules.sh
+++ rules.sh
@@ -15,10 +15,12 @@ iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 # Allowing 172.* addresses (Judge,FTP,PXE,?)
-iptables -A INPUT -s 172.0.0.0/8 -j ACCEPT
-iptables -A OUTPUT -d 172.0.0.0/8 -j ACCEPT
+iptables -A INPUT -s 172.23.0.0/16 -j ACCEPT
+iptables -A OUTPUT -d 172.23.0.0/16 -j ACCEPT
 # Allowing package transmition thourgh localhost for internal jobs
 iptables -A OUTPUT -d 127.0.0.0/8 -j ACCEPT
 iptables -A INPUT -s 127.0.0.0/8 -j ACCEPT
+# Allowing printer
+iptables -A OUTPUT -d 192.168.129.3 -j ACCEPT
 # Saving text version of iptables into file
 iptables-save > /etc/iptables.rules
